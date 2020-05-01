dd386ddef09e7dcb21c0a74f82497accec6fa0a0
Aleksey Semenov <7.on.off@bk.ru>
2020-05-01 14:11:16 +0300

    Adds a solution to the 1 task

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -1,34 +1,36 @@
 from collections import Counter
-
 from django.shortcuts import render_to_response
 
-# Для отладки механизма ab-тестирования используйте эти счетчики
-# в качестве хранилища количества показов и количества переходов.
-# но помните, что в реальных проектах так не стоит делать
-# так как при перезапуске приложения они обнулятся
 counter_show = Counter()
 counter_click = Counter()
 
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    param = request.GET.get('from-landing')
+    if param:
+        counter_click[param] += 1
     return render_to_response('index.html')
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
-    return render_to_response('landing.html')
+    param = request.GET.get('ab-test-arg')
+    variants = {
+        'original': 'landing.html',
+        'test': 'landing_alternate.html'
+    }
+    if param and param in variants:
+        counter_show[param] += 1
+    return render_to_response(variants.get(param) or '')
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Чтобы отличить с какой версии лендинга был переход
-    # проверяйте GET параметр marker который может принимать значения test и original
-    # Для вывода результат передайте в следующем формате:
+
+    def conversion(target):
+        click = counter_click.get(target)
+        show = counter_show.get(target)
+        return round(click / show if show else 0, 2)
+
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': conversion('test'),
+        'original_conversion': conversion('original'),
     })
