51c6db9406d752430821eba9b89db1eddbcb19a2
Aleksey Semenov <7.on.off@bk.ru>
2020-05-01 18:24:37 +0300

    Adds a solution to 2 task

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,3 +1,7 @@
+from csv import DictReader
+from urllib.parse import urlencode
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
 
@@ -7,12 +11,18 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+    with open(settings.BUS_STATION_CSV, encoding='windows-1251') as f:
+        paginator = Paginator([elem for elem in DictReader(f)], 8)
+    current_page = request.GET.get('page', 1)
+    strings = paginator.get_page(current_page)
+    prev_page, next_page = None, None
+    if strings.has_previous():
+        prev_page = f'?{urlencode({"page":strings.previous_page_number()})}'
+    if strings.has_next():
+        next_page = f'?{urlencode({"page":strings.next_page_number()})}'
     return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
+        'bus_stations': strings,
         'current_page': current_page,
-        'prev_page_url': None,
-        'next_page_url': next_page_url,
+        'prev_page_url': prev_page,
+        'next_page_url': next_page,
     })
-
