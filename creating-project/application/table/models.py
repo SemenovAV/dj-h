98074fd67ea8bea12977e9f3f22088c7c5779f52
Aleksey Semenov <7.on.off@bk.ru>
2020-05-24 21:42:25 +0300

    adds models

--- creating-project/application/table/models.py
+++ creating-project/application/table/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Column(models.Model):
+    name = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False)
+    width = models.PositiveSmallIntegerField()
+    ordinal_number = models.PositiveSmallIntegerField()
+
+
+class CsvFilename(models.Model):
+    path = models.FilePathField(
+        path='/',
+        match='^phones.csv$',
+        recursive=False
+    )
