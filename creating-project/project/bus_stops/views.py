baf9fb0f76f60017d0ef293ee64539e26ec551a6
Aleksey Semenov <7.on.off@bk.ru>
2020-05-27 15:42:40 +0300

    adds fixes maps centering

--- creating-project/project/bus_stops/views.py
+++ creating-project/project/bus_stops/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.db.models import Avg
 from django.shortcuts import render
 from django.views import View
 
@@ -9,18 +10,21 @@ class MapView(View):
 
     def get(self, request, *args, **kwargs):
         routes = Route.objects.all().prefetch_related('stations').order_by('name')
-        names = [route['name'] for route in routes.values()]
+        stations_names = [route['name'] for route in routes.values()]
+        active_route_name = request.GET.get('route') or stations_names[0]
+        stations = routes.get(name=active_route_name).stations.values()
+        center = {
+            'y': routes.get(name=active_route_name).stations.aggregate(Avg('latitude'))['latitude__avg'],
+            'x': routes.get(name=active_route_name).stations.aggregate(Avg('longitude'))['longitude__avg']
+        }
         context = {
-            'center': {
-                'y': 55.755814,
-                'x': 37.617635
-            },
-            'routes': names,
+            'center': center,
+            'routes': stations_names,
             'api_key': getattr(settings, "MAP_API_KEY", None)
         }
-        name = request.GET.get('route') or names[0]
-        if name:
-            context['stations'] = routes.get(name=name).stations.values()
-            context['this_route'] = name
+
+        if active_route_name:
+            context['stations'] = stations
+            context['this_route'] = active_route_name
 
         return render(request, 'stations.html', context)
